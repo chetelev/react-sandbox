2e7d9d52e8ddc2057f4d7e0a1399bab774696fa5
Dimitar Chetelev <chetelev@gmail.com>
2022-03-31 15:41:41 +0200

    jsx added, eslint update with new rules

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import Pet from './Pet';
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', { className: 'my-class' }, 'Adopt Me!'),
-    React.createElement(Pet, {
-      name: 'Pipo',
-      animal: 'Dog',
-      breed: 'Mixed Puddle',
-    }),
-    React.createElement(Pet, {
-      name: 'Max',
-      animal: 'Dog',
-      breed: 'American Staffordshire',
-    }),
-    React.createElement(Pet, {
-      name: 'Max',
-      animal: 'Dog',
-      breed: 'French bulldog',
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Pipo" animal="dog" breed="Mixed Puddle"></Pet>
+      <Pet name="Max" animal="dog" breed="American Staffordshire"></Pet>
+      <Pet name="Marli" animal="dog" breed="Golden Retriever"></Pet>
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
